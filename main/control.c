83222a0e26e10d6840d78d3d27fc5be4e04899c6
t0is <tomaseisner69@gmail.com>
2020-10-13 21:17:37 +0200

    fixed vertical set function to turn to both sides correctly

--- main/control.c
+++ main/control.c
@@ -4,13 +4,19 @@
 #include "control.h"
 
 
+float absolute(float value) {
+  if (value < 0)
+    return -value;
+  return value;  
+}
+
 void turn_deg_h (uart_port_t uart_num, int h_dest_deg){
 
     uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
     int temp1, temp2;
     float h_deg, v_deg = -1;
     int len;
-    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <=66);
+    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <= 66);
 
     
     sscanf((int*)data, "%*[^H]H:%4d(%f) V:%4d(%f)", &temp1, &h_deg, &temp2, &v_deg); 
@@ -47,19 +53,20 @@ void turn_deg_h (uart_port_t uart_num, int h_dest_deg){
     
 }
 
-void turn_deg_v (uart_port_t uart_num, int v_dest_deg){
+void turn_deg_v (uart_port_t uart_num, float v_dest_deg){
 
     uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
     int temp1, temp2;
     float h_deg, v_deg = -1;
     
     int len;
-    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <=66);
+    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <= 66);
 
     sscanf((int*)data, "%*[^H]H:%4d(%f) V:%4d(%f)", &temp1, &h_deg, &temp2, &v_deg);
        
     char* d = "";
-    if ((abs(v_deg - v_dest_deg)) < (360-(abs(v_deg - v_dest_deg)))){
+
+    if (v_dest_deg > v_deg && (v_dest_deg-v_deg) <= 180){
         d = "r\n";
     }
     else d = "l\n";
@@ -68,7 +75,7 @@ void turn_deg_v (uart_port_t uart_num, int v_dest_deg){
     while (true) {
         //Read data from the UART
         int len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS);
-
+        
         sscanf((int*)data, "%*[^H]H:%4d(%f) V:%4d(%f)", &temp1, &h_deg, &temp2, &v_deg);
        
 
@@ -82,7 +89,33 @@ void turn_deg_v (uart_port_t uart_num, int v_dest_deg){
         }
 
         printf("%f %f \n\n",h_deg, v_deg);
-
     }
-    
 }
+
+float get_radius_v(uart_port_t uart_num){
+
+    int temp, len;
+    float v_deg;
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <= 66);
+
+    sscanf((int*)data, "%*[^V]V:%4d(%f)", &temp, &v_deg);
+
+    free(data);
+    return v_deg;
+
+}
+
+float get_radius_h(uart_port_t uart_num){
+
+    int temp, len;
+    float h_deg;
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    while((len = uart_read_bytes(uart_num, data, BUF_SIZE, 100 / portTICK_RATE_MS)) <= 66);
+
+    sscanf((int*)data, "%*[^H]H:%4d(%f)", &temp, &h_deg);
+
+    free(data);
+    return h_deg;
+}
+    
