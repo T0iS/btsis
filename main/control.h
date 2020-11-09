df32c5b832148b18cad154877ccf4fa1479251d4
t0is <tomaseisner69@gmail.com>
2020-11-10 00:11:15 +0100

    init

--- main/control.h
+++ main/control.h
@@ -4,11 +4,11 @@
 
 #define BUF_SIZE (1024)
 
-void turn_deg_h (uart_port_t uart_num, int h_dest_deg);
+void turn_deg_h (uart_port_t uart_num, float h_dest_deg);
 void turn_deg_v (uart_port_t uart_num, float v_dest_deg);
 float get_radius_h(uart_port_t uart_num);
 float get_radius_v(uart_port_t uart_num);    
-
+char *str_replace(char *orig, char *rep, char *with);
 
 
 #endif
